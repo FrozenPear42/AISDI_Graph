54116b27f6b47c653a0a010f8bd4863f1edab3f6
Wojciech Gruszka <wojciech@gruszka.eu>
2017-01-15 01:12:30 +0100

    Fixed reading bug.

--- Graph.cpp
+++ Graph.cpp
@@ -1,19 +1,17 @@
 #include <thread>
 #include <algorithm>
 #include <iostream>
+#include <mutex>
 #include "Graph.h"
 
 Graph Graph::fromStream(std::istream& pInput) {
     Graph g;
+    vert_t vA;
+    vert_t vB;
 
     pInput >> g.mVerticesCount;
-
-    while (pInput.good()) {
-        vert_t vA;
-        vert_t vB;
-        pInput >> vA >> vB;
+    while (pInput >> vA >> vB)
         g.mEdges.emplace_back(Edge(vA, vB));
-    }
 
     return g;
 }
@@ -31,31 +29,41 @@ IsomorphicFunction Graph::isomorphicTo(Graph& pOther) {
     if (mVerticesCount <= 20) {
         std::size_t cnt = mVerticesCount;
         std::list<Edge> otherEdges(pOther.mEdges);
-        for (std::size_t i = 0; i < mVerticesCount; ++i) {
-            threads.emplace_back(std::thread([&flag, i, cnt, otherEdges](std::list<Edge> edges) {
-                std::vector<vert_t> vertices;
-                vertices.emplace_back(i);
-                for (std::size_t vert = 0; vert < cnt; ++vert)
-                    if (vert != i)
-                        vertices.emplace_back(vert);
-                do {
-                    for (auto& edge : edges) {
-                        edge.permute(vertices);
-                    }
-                    if (std::is_permutation(edges.begin(), edges.end(), otherEdges.begin()))
-                        flag = 1;
+        std::mutex m;
 
-                    if (flag != 0)
-                        return;
-                } while (std::next_permutation(vertices.begin() + 1, vertices.end()));
-            }, mEdges));
+        for (std::size_t constVertex = 0; constVertex < mVerticesCount; ++constVertex) {
+            threads.emplace_back(
+                    std::thread([&flag, &result, &m, constVertex, cnt, otherEdges](std::list<Edge> selfEdges) {
+                        // Create array of vertices to permute
+                        std::vector<vert_t> vertices;
+                        vertices.emplace_back(constVertex);
+                        for (std::size_t vert = 0; vert < cnt; ++vert)
+                            if (vert != constVertex)
+                                vertices.emplace_back(vert);
+                        // Check every permutation until one is found
+                        do {
+                            std::list<Edge> edges(selfEdges);
+                            for (auto& edge : edges)
+                                edge.permute(vertices);
+
+                            if (std::is_permutation(edges.begin(), edges.end(), otherEdges.begin())) {
+                                std::lock_guard<std::mutex> lock(m);
+                                if (flag == 0) {
+                                    flag = 1;
+                                    for (std::size_t v = 0; v < cnt; ++v) {
+                                        result.addImage((vert_t) v, vertices[v]);
+                                    }
+                                }
+                            }
+                            if (flag != 0)
+                                return;
+                        } while (std::next_permutation(vertices.begin() + 1, vertices.end()));
+                    }, mEdges));
 
         }
-        for (auto&& e : threads)
-            e.join();
+        for (auto&& thread : threads)
+            thread.join();
     }
-    if(flag != 0)
-        result.addImage(1, 1);
     return result;
 }
 
@@ -66,7 +74,7 @@ void IsomorphicFunction::addImage(vert_t pA, vert_t pB) {
 
 std::ostream& operator<<(std::ostream& pOut, IsomorphicFunction& pIsomorphicFunction) {
     for (auto&& e : pIsomorphicFunction.mImage) {
-        pOut << std::to_string(e.first) << " " << std::to_string(e.second) << std::endl;
+        pOut << std::to_string(e.first) << " --> " << std::to_string(e.second) << std::endl;
     }
     return pOut;
 }
